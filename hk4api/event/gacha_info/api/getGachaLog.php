c0cebff3ff37ef46e22c3176d94f4a21e76ac54b
InTheForest <104747445+slzzInTheForest@users.noreply.github.com>
2022-08-28 15:16:05 +0800

    Improve getGachaLog

--- hk4api/event/gacha_info/api/getGachaLog.php
+++ hk4api/event/gacha_info/api/getGachaLog.php
@@ -97,14 +97,23 @@ if (isset($gacha_type)&& isset($gacha_type))
             'region'=>"cn_gf01"
         );
     }
-    echo json_encode($res,JSON_UNESCAPED_UNICODE);
+
 }
 
 else
 {
-    echo <<<EOF
-{"retcode":0,"message":"OK","data":{"page":"1","size":"0","total":"0","list":[],"region":"cn_gf01"}}
-EOF;
+    $res = array(
+        'retcode'=>0,
+        'message'=>"OK",
+        'data'=> array(
+            'page'=>"1",
+            'size'=>"0",
+            'total'=>"0",
+            'list'=>array(),
+            'region'=>"cn_gf01"
+        )
+    );
 }
+echo json_encode($res,JSON_UNESCAPED_UNICODE);
 //gacha_type:301=角色1 302=武器 100=新手 200=常驻 400=角色2
 ?>
